92ad4a8cc932ced02e1b27111110f9e391dddb15
Tertius Scheepers <Tertiussc@gmail.com>
2020-06-13 08:31:48 +0200

    Component component added

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+const ComponentsScreen = () => {
+    const greeting = "Hi there!!!"
+    const bye = <Text>See you later!</Text>
+    return <View>
+        <Text style={styles.textStyle}>This is the components screen</Text>
+        <Text style={{ fontSize: 20 }}>{greeting}</Text>
+        {bye}
+    </View>
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
+    }
+})
+
+export default ComponentsScreen
