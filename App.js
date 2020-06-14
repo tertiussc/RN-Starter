9dcde52a0c8c058133a2b5dfbd030485f713e148
Tertius Scheepers <Tertiussc@gmail.com>
2020-06-14 09:03:29 +0200

    Completed lesson 18

--- App.js
+++ App.js
@@ -3,16 +3,18 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import Components from './src/screens/ComponentsScreen'
 import Excersize1 from './src/screens/Excersize1'
+import ListScreen from './src/screens/ListScreen'
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: Components,
-    Excersize1: Excersize1
+    Excersize1: Excersize1,
+    List: ListScreen
 
   },
   {
-    initialRouteName: "Excersize1",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App"
     }
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+
+const ListScreen = () => {
+
+    const friends = [
+        { name: 'Friend #1' },
+        { name: 'Friend #2' },
+        { name: 'Friend #3' },
+        { name: 'Friend #4' },
+        { name: 'Friend #5' },
+        { name: 'Friend #6' },
+        { name: 'Friend #7' },
+        { name: 'Friend #8' },
+        { name: 'Friend #9' }
+    ]
+    return (
+        <FlatList
+            data={friends}
+            renderItem={({ item }) => {
+                return <Text style={styles.textStyle}>{item.name}</Text>
+            }}
+        ></FlatList>
+    )
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
+    }
+})
+
+export default ListScreen
