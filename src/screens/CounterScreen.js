5f95ce396d76ae632056bddf25cd86d2fe6d164f
Tertius Scheepers <Tertiussc@gmail.com>
2020-07-07 07:56:03 +0200

    cleanup

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -14,13 +14,11 @@ const reducer = (state, action) => {
         default:
             return state
     }
-
 }
 
 const CounterScreen = () => {
 
     const [state, dispatch] = useReducer(reducer, { count: 0 })
-
 
     return (
         <View>
